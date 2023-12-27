f37f32d4f6b965d424e0a7903458978a23b139c3
Prosanto7 <prosantodeb7@gmail.com>
2023-12-27 15:41:22 +0600

    Added template page for each student

--- adminpages/template_report.php
+++ adminpages/template_report.php
@@ -27,32 +27,40 @@ require(__DIR__.'/../../../config.php');
 global $OUTPUT, $DB;
 
 $id = required_param("id", PARAM_INT);// Course_module ID, or.
+$studentid = required_param("studentid", PARAM_INT); // Student ID
+$templateid = required_param("templateid", PARAM_INT); // Template ID
+
 $cm = get_coursemodule_from_id('coversheet', $id, 0, false, MUST_EXIST);
 $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
 
 require_login($course, true, $cm);
 
 $context = context_module::instance($cm->id);
-$PAGE->set_url('/mod/coversheet/adminpages/view_template.php', array('id' => $id));
-$PAGE->set_title("View Template");
+$PAGE->set_url('/mod/coversheet/adminpages/template_report.php', array('id' => $id, 'studentid' => $studentid, 'templateid' => $templateid));
+$PAGE->set_title("Template Report");
 $PAGE->requires->css('/mod/coversheet/mod_coversheet_style.css');
 
 echo $OUTPUT->header();
 
-$template = $DB->get_record('coversheet_templates', array('active' => 1));
-if (!$template) {
-    echo get_string('template_missing', 'coversheet');
-    echo "<a href='upload_template.php?id=$id' class='btn btn-outline-primary'>Back</a>";
-} else {
-    $sql = "SELECT * FROM {coversheet_attempts} ca WHERE ca.cmid = :cmid";
-    $details = $DB->get_records_sql($sql, ['cmid' => $id]);
-    $display = [
-        'cmid' => $id,
-        'templateid' => $template->id,
-        'details' => array_values($details),
-        'webroot' => $CFG->wwwroot
-    ];
-    echo $OUTPUT->render_from_template('mod_coversheet/view_template', $display);
+$template = $DB->get_record('coversheet_templates', array('id' => 1));
+
+$query = "SELECT cc.id as contentid, cc.html FROM {coversheet_contents} cc 
+          WHERE cc.cmid = :cmid";
+$contents = $DB->get_records_sql($query, ['cmid' => $id]);
+
+foreach ($contents as $content) {
+    $html = coversheet_prepare_html_data_for_view($content, $context);
 }
 
+$resource_query = "SELECT * FROM {coversheet_requirements} WHERE cmid = '$id'";
+$resources = $DB->get_records_sql($resource_query);
+
+$display = [
+    'cmid' => $id,
+    'templateid' => $template->id,
+    'contents' => array_values($contents),
+    'resources' => array_values($resources),
+    'webroot' => $CFG->wwwroot
+];
+echo $OUTPUT->render_from_template('mod_coversheet/template_report', $display);
 echo $OUTPUT->footer();
